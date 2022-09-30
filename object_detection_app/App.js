7c0463a2a0b8b15d6a2bf2c2a63cbe5353bedd7a
Sipang K <plaaom39@gmail.com>
2022-10-01 02:21:17 +0700

    fix: fix some dependencies

--- object_detection_app/App.js
+++ object_detection_app/App.js
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
-            <Text>Object Detaction App</Text>
+            <Text>Object Detection App</Text>
             <StatusBar style="auto" />
         </View>
     );
