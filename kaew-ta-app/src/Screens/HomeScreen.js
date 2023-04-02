a18db2cdd6404cda2611d880e014d611250ba602
Sipang K <plaaom39@gmail.com>
2023-04-02 22:45:49 +0700

    Refactor code HomeScreen.js

--- kaew-ta-app/src/Screens/HomeScreen.js
+++ kaew-ta-app/src/Screens/HomeScreen.js
@@ -1,51 +1,53 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
 
 export default function HomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
-            <Text style={{ fontSize: 40, textAlign: 'center', margin: 30 }}>Main Menu</Text>
+            <Text style={{ fontSize: 40, textAlign: "center", margin: 30 }}>
+                Main Menu
+            </Text>
 
             <View style={styles.buttonContainer}>
                 <TouchableOpacity
                     style={styles.button}
-                    onPress={() => navigation.navigate('Obstruction')}
+                    onPress={() => navigation.navigate("Obstruction")}
                     accessible={true} // optional, this is the default
-                    accessibilityLabel={'ปุ่มตรวจจับสิ่งกีดขวาง'} // overrides child content
-                    accessibilityTraits={'Obstruction Detection button'} // only works in ios
-                    accessibilityComponentType={'Obstruction Detection button'} // only works in android
-                    accessibilityHint={'แตะ 2 ครั้ง เพื่อเปิด'}
-                    accessibilityState={{ 'selected': true }}
+                    accessibilityLabel={"ปุ่มตรวจจับสิ่งกีดขวาง"} // overrides child content
+                    accessibilityTraits={"Obstruction Detection button"} // only works in ios
+                    accessibilityComponentType={"Obstruction Detection button"} // only works in android
+                    accessibilityHint={"แตะ 2 ครั้ง เพื่อเปิด"}
+                    accessibilityState={{ selected: true }}
                 >
                     <Text style={styles.textButton}>Obstruction</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.button}
-                    onPress={() => navigation.navigate('Object')}
+                    onPress={() => navigation.navigate("Object")}
                     accessible={true} // optional, this is the default
-                    accessibilityLabel={'ปุ่มตรวจจับวัตถุ'} // overrides child content
-                    accessibilityTraits={'Object Detection button'} // only works in ios
-                    accessibilityComponentType={'Object Detection button'} // only works in android
-                    accessibilityHint={'แตะ 2 ครั้ง เพื่อเปิด'}
-                    accessibilityState={{ 'selected': true }}
+                    accessibilityLabel={"ปุ่มตรวจจับวัตถุ"} // overrides child content
+                    accessibilityTraits={"Object Detection button"} // only works in ios
+                    accessibilityComponentType={"Object Detection button"} // only works in android
+                    accessibilityHint={"แตะ 2 ครั้ง เพื่อเปิด"}
+                    accessibilityState={{ selected: true }}
                 >
                     <Text style={styles.textButton}>Object</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.button}
-                    onPress={() => navigation.navigate('Money')}
+                    onPress={() => navigation.navigate("Money")}
                     accessible={true} // optional, this is the default
-                    accessibilityLabel={'ปุ่มตรวจจับธนบัตร'} // overrides child content
-                    accessibilityTraits={'Money Detection button'} // only works in ios
-                    accessibilityComponentType={'Money Detection button'} // only works in android
-                    accessibilityHint={'แตะ 2 ครั้ง เพื่อเปิด'}
-                    accessibilityState={{ 'selected': true }}
+                    accessibilityLabel={"ปุ่มตรวจจับธนบัตร"} // overrides child content
+                    accessibilityTraits={"Money Detection button"} // only works in ios
+                    accessibilityComponentType={"Money Detection button"} // only works in android
+                    accessibilityHint={"แตะ 2 ครั้ง เพื่อเปิด"}
+                    accessibilityState={{ selected: true }}
                 >
                     <Text style={styles.textButton}>Money & Coin</Text>
                 </TouchableOpacity>
             </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -53,24 +55,24 @@ const styles = StyleSheet.create({
         flex: 1,
         // justifyContent: 'center',
         // alignItems: 'center',
-        backgroundColor: '#fff'
+        backgroundColor: "#fff",
     },
     buttonContainer: {
         flex: 1,
         // backgroundColor: 'pink',
-        flexDirection: 'column',
-        alignItems: 'center',
+        flexDirection: "column",
+        alignItems: "center",
     },
     button: {
-        backgroundColor: 'orange',
+        backgroundColor: "orange",
         borderRadius: 20,
         margin: 10,
         width: 350,
         height: 80,
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: "center",
+        justifyContent: "center",
     },
     textButton: {
         fontSize: 25,
-    }
-})
+    },
+});
