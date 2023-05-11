a49c702d7f5749b83921cb7aec8f86717ae2ef7e
Sipang K <plaaom39@gmail.com>
2023-05-11 12:35:33 +0700

    chore(loadModel): change folder name

--- kaew-ta-app/src/LoadModel/LoadMoneyModel.js
+++ kaew-ta-app/src/LoadModel/LoadMoneyModel.js
@@ -2,15 +2,15 @@ import { loadGraphModel } from "@tensorflow/tfjs";
 import { bundleResourceIO } from "@tensorflow/tfjs-react-native";
 
 // model path
-const modelJson = require('../../assets/model/money_web_model/model.json')
+const modelJson = require('../../assets/model/money_model/model.json')
 const modelWeights = [
-  require('../../assets/model/money_web_model/group1-shard1of7.bin'),
-  require('../../assets/model/money_web_model/group1-shard2of7.bin'),
-  require('../../assets/model/money_web_model/group1-shard3of7.bin'),
-  require('../../assets/model/money_web_model/group1-shard4of7.bin'),
-  require('../../assets/model/money_web_model/group1-shard5of7.bin'),
-  require('../../assets/model/money_web_model/group1-shard6of7.bin'),
-  require('../../assets/model/money_web_model/group1-shard7of7.bin'),
+  require('../../assets/model/money_model/group1-shard1of7.bin'),
+  require('../../assets/model/money_model/group1-shard2of7.bin'),
+  require('../../assets/model/money_model/group1-shard3of7.bin'),
+  require('../../assets/model/money_model/group1-shard4of7.bin'),
+  require('../../assets/model/money_model/group1-shard5of7.bin'),
+  require('../../assets/model/money_model/group1-shard6of7.bin'),
+  require('../../assets/model/money_model/group1-shard7of7.bin'),
 ]
 
 export const loadMoneyModel = async () => {
