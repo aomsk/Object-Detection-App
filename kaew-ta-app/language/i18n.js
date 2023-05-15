d15589830a09edb7cf4a5477267dcd5ee05d969e
Sipang K <plaaom39@gmail.com>
2023-05-15 16:38:30 +0700

    chore(language, labels): change word from 'electric' to 'electric-pole'

--- kaew-ta-app/language/i18n.js
+++ kaew-ta-app/language/i18n.js
@@ -2,7 +2,7 @@ import { I18n } from "i18n-js";
 export const i18n = new I18n({
   en: {
     billBoard: "BillBoard",
-    electric: "Electric",
+    electric: "Electric pole",
     sofa: "Sofa",
     table: "Table",
     cup: "Cup",
