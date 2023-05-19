c12cb8e26cd699f0937eb6d5fb1920bf352a350e
Sipang K <plaaom39@gmail.com>
2023-05-19 12:28:30 +0700

    refactor: remove unused code

--- kaew-ta-app/src/Camera/CameraObjectView.js
+++ kaew-ta-app/src/Camera/CameraObjectView.js
@@ -10,7 +10,7 @@ import { getLocales } from 'expo-localization';
 
 const TensorCamera = cameraWithTensors(Camera);
 
-const CameraObjectView = ({ ctx, model, inputTensorSize, type, config }) => {
+const CameraObjectView = ({ model, inputTensorSize, config }) => {
   const [className, setClassName] = useState('')
 
   // Check local language in device
